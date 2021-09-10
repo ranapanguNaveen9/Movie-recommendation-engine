4ea555704ead6deda3d67ff063e8b6258393fc7c
ranapanguNaveen9 <58222286+ranapanguNaveen9@users.noreply.github.com>
2021-09-10 06:57:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import flask
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+
+# In[2]:
+
+
+app = flask.Flask(__name__, template_folder='Templates')
+
+
+# In[3]:
+
+
+# read the data
+bollywood = pd.read_csv("Bollywood_movies.csv")
+kollywood = pd.read_csv("Kollywood_movies.csv")
+mollywood = pd.read_csv("Mollywood_movies.csv")
+tollywood = pd.read_csv("Tollywood_movies.csv")
+sandalwood = pd.read_csv("Sandalwood_movies.csv")
+
+
+# In[4]:
+new_list = [bollywood,kollywood,mollywood,tollywood,sandalwood]
+all_movies = pd.concat(new_list,ignore_index = True,sort = False)
+df = all_movies.reset_index()
+indices = pd.Series(df.index, index=df['Title'])
+all_titles = [df['Title'][i] for i in range(len(df['Title']))]
+df = df.fillna("")
+df["Comb"] = df['Cast']+' '+df['Title'] +' '+ df['Director']+' '+df['Genre']+' '+df['Music'] +' '+ df['language']
+
+tfidf = TfidfVectorizer(stop_words='english')
+
+tfidf_matrix = tfidf.fit_transform(df['Comb'])
+
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+
+# In[5]:
+
+
+def recommend_movies(movie_name):
+    idx = indices[movie_name]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    movies = df.iloc[movie_indices][['Title']]
+    return movies
+
+
+# In[9]:
+
+
+@app.route('/', methods=['GET', 'POST'])
+
+
+# In[ ]:
+
+
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('index.html'))
+
+    if flask.request.method == 'POST':
+        m_name = flask.request.form['movie_name']
+        m_name = m_name.title()
+    if m_name not in all_titles:
+        return(flask.render_template('no.html'))
+    else:
+        result = recommend_movies(m_name)
+        names = []
+        year = []
+        score = []
+        lang = []
+        for i in range(len(result)):
+            names.append(result.iloc[i][0])
+            year.append(result.iloc[i][1])
+            score.append(result.iloc[i][2])
+            lang.append(result.iloc[i][3])
+
+    return flask.render_template('yes.html', movie_names=result,search_name=m_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
