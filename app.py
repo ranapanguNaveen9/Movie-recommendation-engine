551df9a3c3d5272d670f495bdc57439f3adef246
ranapanguNaveen9 <58222286+ranapanguNaveen9@users.noreply.github.com>
2021-09-11 15:04:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,13 @@ app = flask.Flask(__name__, template_folder='Templates')
 #bollywood = pd.read_csv("Bollywood_movies.csv")
 #kollywood = pd.read_csv("Kollywood_movies.csv")
 #mollywood = pd.read_csv("Mollywood_movies.csv")
-#tollywood = pd.read_csv("Tollywood_movies.csv")
+tollywood = pd.read_csv("Tollywood_movies.csv")
 #sandalwood = pd.read_csv("Sandalwood_movies.csv")
 #new_list = [bollywood,kollywood,mollywood,tollywood,sandalwood]
 #all_movies = pd.concat(new_list,ignore_index = True,sort = False)
+#all_movies = pd.read_csv("movies.csv")
 
-all_movies = pd.read_csv("movies.csv")
-
-df = all_movies.reset_index()
+df = tollywood.reset_index()
 indices = pd.Series(df.index, index=df['Title'])
 all_titles = [df['Title'][i] for i in range(len(df['Title']))]
 df = df.fillna("")
